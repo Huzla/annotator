e06af7dede4c06d9be45bc255ab997166259c7af
laal <lari.alakukku@patria.fi>
2021-09-14 15:43:54 +0300

    Added annotation model

--- src/models.py
+++ src/models.py
@@ -9,6 +9,7 @@ class Domain(db.Model):
     name = db.Column(db.String())
     index_page = db.Column(db.String())
     groups = db.Column(db.Integer)
+    annotations = db.relationship("Annotation", backref="domain", lazy=True, cascade="all, delete")
 
     def __init__(self, name, index_page, groups):
         self.name = name
@@ -17,3 +18,22 @@ class Domain(db.Model):
 
     def __repr__(self):
         return f"<Domain { self.name } with { self.groups } groups>"
+
+
+class Annotation(db.Model):
+    __tablename__ = "annotation"
+
+    id = db.Column(db.Integer, primary_key=True)
+    domain = db.Column(db.Integer, db.ForeignKey("domain.id"))
+    url = db.Column(db.String())
+    group = db.Column(db.Integer)
+    classes = db.Column(db.String())
+
+    def __init__(self, domain, url, group, classes):
+        self.domain = domain
+        self.url = url
+        self.group = group
+        self.classes = classes
+
+    def __repr__(self):
+        return f"<Annotation at { self.url } of group { self.group } >"
